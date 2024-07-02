6bb9e04a474f9d482c3cabaa2e607f777589ad0e
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-07-02 16:17:04 +0530

    topic animation

--- main.js
+++ main.js
@@ -3,20 +3,15 @@ const scriptgsap = document.createElement('script');
 scriptgsap.src = './js/gsap.min.js';
 scriptgsap.setAttribute('type', 'text/javascript');
 
-
-const data2 = {
-  Sport: ['1 Sport1', '2 Sport2', '3 Sport3', '4 Sport4', '5 Sport5',],
-  National: ['1 National1', '2 National2', '3 National3', '4 National4', '5 National5',],
-  Regional: ['1 Regional1', '2 Regional2', '3 Regional3', '4 Regional4', '5 Regional5',],
+let data2 = {
+  'Sport': ['1 Sport1', '2 Sport2', '3 Sport3', '4 Sport4', '5 Sport5',],
+  'National': ['1 National1', '2 National2', '3 National3', '4 National4', '5 National5',],
+  'Regional': ['1 Regional1', '2 Regional2', '3 Regional3', '4 Regional4', '5 Regional5',],
 }
 
-
-
-let categories = Object.keys(data2);
-let currentCategoryIndex = 0;
-let messages = [...data2[categories[currentCategoryIndex]]];
-
-
+let categories;
+let currentCategoryIndex;
+let messages;
 // let messages = [];
 var _speed = 500;
 var _gap = 50;
@@ -26,38 +21,53 @@ const nickbMethod = () => {
   _counter = 1;
   _stopatcounter = 0;
 
-  // const _speed = 500;
   const _screen = 1920;
-  // let messages = data1;
+  function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
 
+  const updatedata=()=>{
+    categories = Object.keys(data2);
+    currentCategoryIndex = 0;
+
+
+    
+  }
   function start() {
+    updatedata()
+    messages = [...data2[categories[currentCategoryIndex]]];
     updatestring('heading', Object.keys(data2)[currentCategoryIndex])
+    gsap.set(document.getElementById('heading'), { x: -510 });
+    let timeline2 = gsap.timeline({ paused: true });
+    timeline2.to('#heading', {
+      duration: 0.3,
+      x: 160,
+      ease: 'none',
+    });
+    timeline2.play();
+
+    gsap.set(document.getElementById('scroll_strip'), { x: -2100 });
+    let timeline3 = gsap.timeline({ paused: true });
+    timeline2.to('#scroll_strip', {
+      duration: 0.5,
+      x: 400,
+      ease: 'none',
+    });
+    timeline3.play();
+
     _onAir = true;
     next();
   }
-  window.start = start;
-
   function stop() {
     _onAir = false;
-    // console.log(_counter);
     _stopatcounter = _counter;
   }
+  window.start = start;
+  window.updatedata = updatedata;
   window.stop = stop;
-
-
   function next() {
-    // let it run to end if not on air
-
-
     if (!_onAir) return;
-
-
-
     _counter++;
-    // console.log(messages.length)
-
-
-
     var originalGroup = document.getElementById('scroll');
     originalGroup
       .getElementsByTagName('text')[0]
@@ -71,7 +81,11 @@ const nickbMethod = () => {
       .getElementsByTagName('text')[0]
       .getElementsByTagName('tspan')[0].textContent = nextMsg;
 
-    document.getElementsByTagName('svg')[0].appendChild(nextDiv);
+    var svg = document.getElementsByTagName('svg')[0];
+    var referenceNode = svg.children[2];
+
+    insertAfter(nextDiv, referenceNode);
+
     let msgWidth = nextDiv.getBBox().width;
     nextDiv
       .getElementsByTagName('text')[0]
@@ -84,29 +98,21 @@ const nickbMethod = () => {
       ease: 'none',
     });
 
- 
-
     timeline.play();
-    // timeline.eventCallback('onComplete', offScreen, [nextDiv.id]);
     timeline.eventCallback('onComplete', () => {
       offScreen(nextDiv.id);
-
     });
 
-
     if (messages.length === 0) {
-
       timeline.call(() => {
         currentCategoryIndex = (currentCategoryIndex + 1) % categories.length;
         messages = [...data2[categories[currentCategoryIndex]]];
         updatestring('heading', categories[currentCategoryIndex]);
-
-        gsap.set(document.getElementById('heading_g'), { x: -210 });
+        gsap.set(document.getElementById('heading'), { x: -510 });
         let timeline2 = gsap.timeline({ paused: true });
-    
-        timeline2.to('#heading_g', {
+        timeline2.to('#heading', {
           duration: 0.2,
-          x: 200,
+          x: 160,
           ease: 'none',
         });
         timeline2.play();
@@ -117,8 +123,6 @@ const nickbMethod = () => {
     else {
       timeline.call(next, [], getNextMsgTime(msgWidth));
     }
-
-
   }
 
   function getDuration(width) {
@@ -146,7 +150,6 @@ const nickbMethod = () => {
         .removeChild(document.getElementById('scroll_strip'));
   }
 
-  // start();
 };
 
 scriptgsap.onload = function () {
